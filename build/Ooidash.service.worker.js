b603e7dfaa070a6066f4d2b3d9d8deec9ea637f9
Samuel Carbone <scarbone1998@gmail.com>
2024-10-04 18:23:30 -0400

    release mode build test

--- build/Ooidash.service.worker.js
+++ build/Ooidash.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1728010214|86935647354';
+const CACHE_VERSION = '1728080605|16836876644';
 /** @type {string} */
 const CACHE_PREFIX = 'Ooidash-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
