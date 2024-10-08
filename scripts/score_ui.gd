485db468246f3cbdf99c928c38dbaa2054fe6d12
Samuel Carbone <scarbone1998@gmail.com>
2024-10-08 19:52:07 -0400

    fps counter

--- scripts/score_ui.gd
+++ scripts/score_ui.gd
@@ -1,8 +1,10 @@
 extends Control
 
 @onready var game_manager = get_node('/root/MainScene/GameManager')
-@onready var label: Label = $Label
+@onready var label: Label = $VBoxContainer/Label
+@onready var fps: Label = $VBoxContainer/FPS
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	label.text = "Score: %d" % [game_manager.player_score]
+	fps.text = "FPS: %d" % [Engine.get_frames_per_second()]
