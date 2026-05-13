d96efc164e1d69476043dc68d6a1128a91f36d8e
Samuel Carbone <scarbone1998@gmail.com>
2026-05-12 23:11:09 -0700

    Add music toggle to menu

--- build/Ooidash.service.worker.js
+++ build/Ooidash.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1778652178|1821604';
+const CACHE_VERSION = '1778652612|2008977';
 /** @type {string} */
 const CACHE_PREFIX = 'Ooidash-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,17 +1,40 @@
 extends Control
 
+const SETTINGS_PATH := "user://settings.cfg"
+
 var main_scene: PackedScene = preload("res://main_scene.tscn")
+var music_enabled := true
 
-# Called when the node enters the scene tree for the first time.
+@onready var music_toggle: CheckButton = $VBoxContainer/MusicRow/MusicToggle
+
 func _ready() -> void:
-	pass # Replace with function body.
+	_load_settings()
+	music_toggle.set_pressed_no_signal(music_enabled)
+	_apply_music_setting()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _load_settings() -> void:
+	var config = ConfigFile.new()
+	var error = config.load(SETTINGS_PATH)
+	if error == OK:
+		music_enabled = config.get_value("audio", "music_enabled", true)
 
 
+func _save_settings() -> void:
+	var config = ConfigFile.new()
+	config.set_value("audio", "music_enabled", music_enabled)
+	config.save(SETTINGS_PATH)
+
+
+func _apply_music_setting() -> void:
+	var master_bus = AudioServer.get_bus_index("Master")
+	AudioServer.set_bus_mute(master_bus, not music_enabled)
 
 func _on_start_button_button_down() -> void:
 	get_tree().change_scene_to_packed(main_scene)
+
+
+func _on_music_toggle_toggled(toggled_on: bool) -> void:
+	music_enabled = toggled_on
+	_apply_music_setting()
+	_save_settings()
